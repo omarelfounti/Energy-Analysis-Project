eb149f1f699021db8d4fee2a917e8ad1c4e40c5a
omarelfounti <elfountiomar@gmail.com>
2024-10-31 21:57:20 +0100

    app file

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+from utils.data_loader import load_data
+from utils.data_processing import group_data
+from utils.visualizations import plot_metrics
+
+
+file1_path = 'data/sahkon-hinta-010121-240924.csv'
+file2_path = 'data/Electricity_20-09-2024.csv'
+merged_df = load_data(file1_path, file2_path)
+
+
+st.sidebar.header("Select Analysis Parameters")
+start_date = st.sidebar.date_input("Select Start Date", value=pd.to_datetime("2021-01-01"))
+end_date = st.sidebar.date_input("Select End Date", value=pd.to_datetime("2024-09-24"))
+interval = st.sidebar.selectbox("Select Grouping Interval", ("Daily", "Weekly", "Monthly"))
+
+
+filtered_df = merged_df[(merged_df['Date'] >= pd.to_datetime(start_date)) & 
+                        (merged_df['Date'] <= pd.to_datetime(end_date))]
+
+
+grouped_df = group_data(filtered_df, interval)
+
+
+st.header(f"{interval} Summary from {start_date} to {end_date}")
+st.write(grouped_df)
+
+
+fig = plot_metrics(grouped_df, interval)
+st.pyplot(fig)
